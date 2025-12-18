0e0a84d454e2475108b3a1760324e360477628d1
Kaka Dimas Soehendra  Putra <kakadimas01@gmail.com>
2025-12-18 22:42:25 +0700

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -61,7 +61,7 @@ export async function POST(req: Request) {
     ]
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
